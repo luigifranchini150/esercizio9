d3ac81de1d5785f65f91ec6ab2dc6cd2eab86dda
luigifranchini150 <luigifranchini150@gmail.com>
2025-06-19 16:45:39 +0200

    Add files via upload

---
+++ selfwork arry 1/selfwork arry 1.js
@@ -0,0 +1,11 @@
+let numbers = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4]
+
+let sarted = numbers.sort((a, b)=> a - b)
+console.log(sarted);
+
+//let sarted2 = numbers.sort((a, b)=> b - a)
+//console.log(sarted2);
+
+let sarted2 = sarted.reverse()
+console.log(sarted2);
+
